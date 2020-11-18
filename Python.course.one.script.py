3f19030bc0a23e767472fbc623793015cd681d2d
Susmita Aown <sa1915@student.anglia.ac.uk>
2020-11-18 21:09:15 +0000

    Commiting my first python script for the Python basics course

---
+++ Python.course.one.script.py
@@ -0,0 +1,119 @@
+print("Bye World")
+
+# Check the Python version
+
+import sys
+print(sys.version)
+
+# Print string and error to see the running order
+print("This will be printed")
+frint("This will cause an error")
+print("This will NOT be printed"
+
+sys.float_info
+
+type(0.56)
+
+# convert object type = typecasting
+
+# verify that this is an integer
+type(2)
+
+# convert 2 to a float
+float(2)
+
+# convert integer 2 to a float and check its type
+type(float(2))
+
+# casting 1.1 to integer will result in loss of information
+int(1.1)
+
+# convert a string into an integer 
+int("1")
+
+# convert a string into an integer with error
+int("1 or 2 people")
+
+# convert the string "1.2" into a float
+float("1.2")
+
+# convert an integer to a string
+str(1)
+
+# convert a float to a string
+str(1.2)
+
+# type of True
+type(True)
+type(False)
+
+# Convert True to int
+int(True)
+
+# convert 1 and 0 to boolean
+bool(1)
+bool(0)
+
+# convert True to float
+float(True)
+
+# What is the data type of the result of: 6 / 2?
+type(6/3)
+
+# What is the type of the result of: 6 // 2? (Note the double slash //.)
+type(6//2)
+
+# Addition operation expression
+43 + 60 + 16 + 41
+
+# Multiplication operation expression
+
+5 * 5
+
+# Division operation expression
+
+25 / 5
+# Division operation expression
+
+25 / 6
+# Integer division operation expression
+
+25 // 5
+
+# Integer division operation expression
+
+25 // 6
+
+# Mathematical expression
+
+30 + 2 * 60
+
+# Mathematical expression
+(30 + 2) * 60
+
+# Store value into variable
+x = 43 + 60+ 16+ 41
+
+# Print out the value in variable
+
+x
+
+# Use another variable to store the result of the operation between variable and value
+
+y = x / 60
+
+# overwrite variable with new value
+x = x/60
+x
+# Name the variables meaningful
+total_min = 43 + 42 +57 # Total length of albums in min
+total_min
+
+# Name the variables meaningfully 
+total_hours = total_min /60 # Total length of albums in hours
+total_hours
+
+# Complicate expression
+
+total_hours = (43 + 42 + 57) / 60  # Total hours in a single expression
+total_hours
